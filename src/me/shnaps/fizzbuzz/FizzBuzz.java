f16793ae7a0d690a5b409ef09fefd8bd6c4d8377
shnaps <shnapsx@gmail.com>
2019-02-06 21:50:54 +0300

    Update SOUT message

--- src/me/shnaps/fizzbuzz/FizzBuzz.java
+++ src/me/shnaps/fizzbuzz/FizzBuzz.java
@@ -16,19 +16,19 @@ public class FizzBuzz {
         if (i > 100) {
             return;
         }
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            System.out.println(i + " FizzBuzz");
+        if (i % 15 == 0) {
+            System.out.println("Number is: " +  i + " -- FizzBuzz");
         } else {
             if (i % 3 == 0) {
-                System.out.println(i + " Fizz");
+                System.out.println("Number is: " +  i + " -- Fizz");
             } else {
                 if (i % 5 == 0) {
-                    System.out.println(i + " Buzz");
+                    System.out.println("Number is: " +  i + " -- Buzz");
                 }
             }
         }
         if (i % 3 != 0 && i % 5 != 0) {
-            System.out.println(i);
+            System.out.println("Number is: " + i);
         }
 
         fizzBuzzLoad(i + 1);
